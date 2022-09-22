8626d48ae4cf0a572e5c915664efcb287ba04489
Adelin Sofrag <adelin.sofrag@gmail.com>
2022-09-22 14:01:10 +0300

    Assets update.

--- constants/icons.js
+++ constants/icons.js
@@ -2,7 +2,7 @@ const Aeropress = require("./../assets/icons/V1.0.1-Aeropress.png");
 const Dripper = require("./../assets/icons/V1.0.1-Dripper.png");
 const Grinder = require("./../assets/icons/V1.0.1-Grinder.png");
 const Ratio = require("./../assets/icons/justice-scale.png");
-const Water = require("./../assets/icons/water.png");
+const Water = require("./../assets/icons/V1.0.1-Water.png");
 const Default = require("./../assets/icons/default.png");
 const Logo = require("./../assets/V1.0.1-Logo.png");
 
